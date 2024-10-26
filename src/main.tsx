517052fb78d8c7dc8a3b0e64b472706339d21b4a
novakf <bogdannovikov116@mail.ru>
2024-10-26 18:22:31 +0300

    upload

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
